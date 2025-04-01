8a3767e3165d01904a729daeb4d595b2807827b1
SubAkBa <circle5926@gmail.com>
2025-04-01 19:10:36 +0900

    initial commit

---
+++ src/main/kotlin/com/example/roomescapebe/RoomEscapeBeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.roomescapebe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoomEscapeBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoomEscapeBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/roomescapebe/RoomEscapeBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.roomescapebe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoomEscapeBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
